22db3391635f0d59cb05a2470814c43e7d44448d
Jurgita Ratkeliene <jurgita.ratkeliene@codeacademylt.onmicrosoft.com>
2022-10-01 22:50:56 +0300

    Pirma uzduotis

---
+++ 1/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript',
+  ],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./1/tsconfig.json'],
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [0],
+  },
+};
