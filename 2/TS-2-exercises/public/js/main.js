20689bb13c3e93c3ed9cb20c94e991984fd5ee83
Jurgita Ratkeliene <jurgita.ratkeliene@codeacademylt.onmicrosoft.com>
2022-10-02 11:10:20 +0300

    Antros - ketvirta dalis

--- 2/TS-2-exercises/public/js/main.js
+++ 2/TS-2-exercises/public/js/main.js
@@ -1,10 +1,27 @@
 "use strict";
 console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
 {
+    const drinks = [
+        { name: "lemonade", price: 50 },
+        { name: "lime", price: 10 }
+    ];
+    function sortDrinkByPrice(array) {
+        return array.sort((n1, n2) => n1.price - n2.price);
+    }
+    console.log({
+        Original: drinks,
+        Sorted: sortDrinkByPrice([...drinks])
+    });
 }
 console.groupEnd();
 console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
+    const addName = (stolen, stolenName, stolenValue) => (Object.assign(Object.assign({}, stolen), { [stolenName]: stolenValue }));
+    console.log({
+        'addName({}, "Brutus", 300)': addName({}, "Brutus", 300),
+        'addName({ piano: 500 }, "Brutus", 400)': addName({ piano: 500 }, "Brutus", 400),
+        'addName({ piano: 500, stereo: 300 }, "Caligula", 440)': addName({ piano: 500, stereo: 300 }, "Caligula", 440)
+    });
 }
 console.groupEnd();
 //# sourceMappingURL=main.js.map
--- 2/TS-2-exercises/src/main.ts
+++ 2/TS-2-exercises/src/main.ts
@@ -1,11 +1,44 @@
 console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
 {
 
+    type drink = {
+        name: string,
+        price: number
+      };
+
+    const drinks:drink[] = [
+        {name: "lemonade", price: 50},
+        {name: "lime", price: 10}
+      ]
+     function  sortDrinkByPrice(array:drink[]):drink[]{
+
+        return  array.sort((n1, n2) => n1.price - n2.price);
+  
+      }
+console.log({
+    Original: drinks,
+    Sorted:sortDrinkByPrice([...drinks])
+  });
 }
 console.groupEnd();
 
 console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
+
+    type stolenItems = {
+        [name:string]: number,
+      };
+      const addName = (stolen:stolenItems, stolenName:string, stolenValue:number):stolenItems =>({
+        ...stolen,
+        [stolenName]: stolenValue
+      });
+
+      console.log({
+        'addName({}, "Brutus", 300)':addName({}, "Brutus", 300),
+        'addName({ piano: 500 }, "Brutus", 400)':addName({ piano: 500 }, "Brutus", 400),
+        'addName({ piano: 500, stereo: 300 }, "Caligula", 440)':addName({ piano: 500, stereo: 300 }, "Caligula", 440)
+      });
+
 
 }
 console.groupEnd();
