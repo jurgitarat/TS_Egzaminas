51b78fcb66d028f84946bd3c4b2c18793dae3426
Jurgita Ratkeliene <jurgita.ratkeliene@codeacademylt.onmicrosoft.com>
2022-10-02 22:08:58 +0300

    7 uzduotis pradzia

---
+++ 7/public/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+console.group('1. Sukurkitę sąrašo mazgo struktūrą ListNode, bet kokiam duomenų tipui');
+{
+}
+console.groupEnd();
+console.group('2. Sukurkite sąrašo klasę List');
+{
+}
+console.groupEnd();
+console.group('3. Sukurkite metodą pridėti elementui į sąrašo priekį.');
+{
+}
+console.groupEnd();
+console.group('4. Sukurkite metodą pridėti elementui į sąrašo galą.');
+{
+}
+console.groupEnd();
+console.group('5. Sukurkite metodą List.forEach klasėje List, kuris vykdytų parametru perduotą funkciją');
+{
+}
+console.groupEnd();
+//# sourceMappingURL=main.js.map
