41f98701ecb9f623b4d6787cac916fe178d66544
Jurgita Ratkeliene <jurgita.ratkeliene@codeacademylt.onmicrosoft.com>
2022-10-02 15:09:28 +0300

    3-9 uzduotis

--- 3/public/js/main.js
+++ 3/public/js/main.js
@@ -72,18 +72,62 @@ console.group('5. Parašykite funkciją, kuri atspausdina masyvo elementus atbul
 console.groupEnd();
 console.group('6. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus vienoje eilutėje: 0 1 2 3 ...');
 {
+    function masyvoIndeksaiEiluteje(masyvas) {
+        let sarasas = "";
+        for (let n = 0; n < masyvas.length; n++)
+            sarasas = sarasas + n + " ";
+        console.log(sarasas);
+    }
+    const Bandymas1 = ["Jonas", "Petras", "Antanas"];
+    const Bandymas2 = [5, 6, 3, 7, 3, 1, 3, 5];
+    console.log(Bandymas1);
+    masyvoIndeksaiEiluteje(Bandymas1);
+    console.log(Bandymas2);
+    masyvoIndeksaiEiluteje(Bandymas2);
 }
 console.groupEnd();
 console.group('7. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes vienoje eilutėje: -111 2 -9 48 ...');
 {
+    function masyvoElementaiEiluteje(masyvas) {
+        console.log(masyvas.join(" "));
+    }
+    const Bandymas1 = ["Jonas", "Petras", "Antanas"];
+    const Bandymas2 = [5, 6, 3, 7, 3, 1, 3, 5];
+    console.log(Bandymas1);
+    masyvoElementaiEiluteje(Bandymas1);
+    console.log(Bandymas2);
+    masyvoElementaiEiluteje(Bandymas2);
 }
 console.groupEnd();
 console.group('8. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras vienoje eilutėje, tokiu formatu:');
 {
+    function masyvasEiluteje(masyvas) {
+        let sarasas = "";
+        for (let n = 0; n < masyvas.length; n++)
+            sarasas = sarasas + "[" + n + "]=>" + masyvas[n] + ", ";
+        console.log(sarasas);
+    }
+    const Bandymas1 = ["Jonas", "Petras", "Antanas"];
+    const Bandymas2 = [5, 6, 3, 7, 3, 1, 3, 5];
+    console.log(Bandymas1);
+    masyvasEiluteje(Bandymas1);
+    console.log(Bandymas2);
+    masyvasEiluteje(Bandymas2);
 }
 console.groupEnd();
 console.group('9. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo elementų padaugintų iš 2');
 {
+    function masyvasEiluteje(masyvas) {
+        const padidintasMasyvas = [...masyvas];
+        for (let n = 0; n < padidintasMasyvas.length; n++)
+            padidintasMasyvas[n] = padidintasMasyvas[n] * 2;
+        return padidintasMasyvas;
+    }
+    const Bandymas2 = [5, 6, 3, 7, 3, 1, 3, 5];
+    console.log({
+        PradinisMasyvas: Bandymas2,
+        PadidintasMasyvas: masyvasEiluteje(Bandymas2)
+    });
 }
 console.groupEnd();
 console.group('10. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo elementų pakeltų kvadratu');
--- 3/src/main.ts
+++ 3/src/main.ts
@@ -104,12 +104,41 @@ console.groupEnd();
 console.group('6. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus vienoje eilutėje: 0 1 2 3 ...');
 {
   // Užduoties sprendimas ir užduoties sprendimo atspausdinimas su duomenimis
+  function masyvoIndeksaiEiluteje(masyvas:any[]):void{
+    let sarasas :string="";
+    for (let n = 0; n<masyvas.length;n++)
+      sarasas=sarasas+n+" ";
+    
+    console.log(sarasas);   
+}
+const Bandymas1 = ["Jonas", "Petras", "Antanas"];
+const Bandymas2 = [5,6,3,7,3,1,3,5];
+console.log(Bandymas1);
+masyvoIndeksaiEiluteje(Bandymas1);
+console.log(Bandymas2);
+masyvoIndeksaiEiluteje(Bandymas2);
+
+
+
 }
 console.groupEnd();
 
 console.group('7. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes vienoje eilutėje: -111 2 -9 48 ...');
 {
   // Užduoties sprendimas ir užduoties sprendimo atspausdinimas su duomenimis
+  // Užduoties sprendimas ir užduoties sprendimo atspausdinimas su duomenimis
+  function masyvoElementaiEiluteje(masyvas:any[]):void{
+    console.log(masyvas.join(" "));   
+}
+const Bandymas1 = ["Jonas", "Petras", "Antanas"];
+const Bandymas2 = [5,6,3,7,3,1,3,5];
+console.log(Bandymas1);
+masyvoElementaiEiluteje(Bandymas1);
+console.log(Bandymas2);
+masyvoElementaiEiluteje(Bandymas2);
+
+
+
 }
 console.groupEnd();
 
@@ -117,12 +146,44 @@ console.group('8. Parašykite funkciją, kuri atspausdina kiekvieno masyvo eleme
 //  [0]=>17, [1]=>8, [2]=>88 ..
 {
   // Užduoties sprendimas ir užduoties sprendimo atspausdinimas su duomenimis
+  function masyvasEiluteje(masyvas:any[]):void{
+    let sarasas :string="";
+    for (let n = 0; n<masyvas.length;n++)
+      sarasas=sarasas+"["+n+"]=>"+masyvas[n]+", ";
+    
+    console.log(sarasas);   
+}
+const Bandymas1 = ["Jonas", "Petras", "Antanas"];
+const Bandymas2 = [5,6,3,7,3,1,3,5];
+console.log(Bandymas1);
+masyvasEiluteje(Bandymas1);
+console.log(Bandymas2);
+masyvasEiluteje(Bandymas2);
+
+
+
+
+
+
 }
 console.groupEnd();
 
 console.group('9. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo elementų padaugintų iš 2');
 {
   // Užduoties sprendimas ir užduoties sprendimo atspausdinimas su duomenimis
+
+
+  function masyvasEiluteje(masyvas:number[]):number[]{
+    const padidintasMasyvas:number[]=[...masyvas];
+    for (let n = 0; n<padidintasMasyvas.length;n++)
+    padidintasMasyvas[n]=padidintasMasyvas[n]*2;
+    
+    return padidintasMasyvas
+}
+const Bandymas2 = [5,6,3,7,3,1,3,5];
+console.log({
+  PradinisMasyvas: Bandymas2,
+  PadidintasMasyvas: masyvasEiluteje(Bandymas2)})
 }
 console.groupEnd();
 
